567a287c0ea7e64e2125e59452022ee36210c25d
Svetlana <Svetlanat1005@yandex.ru>
2024-11-07 16:29:37 +0300

    add StudentGroupService.java

---
+++ StudentGroupService.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroupService {
+
+    private StudentGroup studentGroup;
+
+    public void removeStudentByFIO (String firstName, String lastName, String middleName) {
+        
+        Iterator<Student> iterator = studentGroup.iterator();
+        while(iterator.hasNext()){
+            Student student = iterator.next();
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName) && student.getMiddleName().equals(lastName)){
+                iterator.remove();
+            }
+        }
+    }
+
+    public List<Student> getSortedStudentList() {
+        List<Student> studentList = new ArrayList<>(studentGroup.getStudentList());
+        Collections.sort(studentList);
+        return studentList;
+    }
+
+    public List<Student> getSortedStudentByFIO() {
+        List<Student> studentList = new ArrayList<>(studentGroup.getStudentList());
+        studentList.sort(new UserComparator<Student>());
+        return studentList;
+    }
+
+    public void createStudent(String fistName, String lastName, String middleName){
+        studentGroup.createStudent(fistName, lastName, middleName);
+    }
+
+}
